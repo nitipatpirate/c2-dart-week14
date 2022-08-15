1503e2824685bb31d9d49594ba6cc44215bc4579
nitipatpirate <nitipatkongswat99@gmail.com>
2022-08-15 11:15:54 +0700

    first commit

---
+++ bin/dart_week14.dart
@@ -0,0 +1,8 @@
+void main() {
+  int sum = add(5,4,3);
+  print(sum);
+}
+
+int add(int a, int b, int c) {
+  return a-b-c;
+}
---
+++ bin/function101.dart
@@ -0,0 +1,9 @@
+void main (){
+  add();
+}
+
+add (){
+  int a = 10;
+  int b = 5;
+  print(a+b);
+}
